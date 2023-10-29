b2e218af54644d0acea59e17c58b6c6c94368242
Chris Rinaldi <crinaldi44@gmail.com>
2023-10-29 17:15:33 -0400

    default api basepath change

--- crawling-suite/ftm_crawling_suite/pipelines.py
+++ crawling-suite/ftm_crawling_suite/pipelines.py
@@ -35,7 +35,7 @@ class MongoDBSingleton:
         if MongoDBSingleton.__instance is not None:
             raise Exception("This class is a singleton!")
         else:
-            mongo_uri_encoded = os.environ.get('MONGO_URI_PROD_ENCODED', 'bW9uZ29kYjovL2xvY2FsaG9zdDoyNzAxNy8=')
+            mongo_uri_encoded = os.environ.get('MONGO_URI_ENCODED', 'bW9uZ29kYitzcnY6Ly9hZG1pbjpla3kwUFF5TjNjZDcxV3dZQGNsdXN0ZXIwLmlsbHFoLm1vbmdvZGIubmV0')
             mongo_uri = base64.b64decode(mongo_uri_encoded).decode('utf-8')
             MongoDBSingleton.__instance = pymongo.MongoClient(mongo_uri)
 
